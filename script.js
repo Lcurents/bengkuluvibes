fc3f9fdb628d79433e9404b3e01804b6a7abe31e
Lcurents <laurentiusdika28@gmail.com>
2023-12-02 22:56:51 +0700

    final home

--- script.js
+++ script.js
@@ -2,15 +2,22 @@ const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     console.log(entry);
     if (entry.isIntersecting) {
-      entry.target.classList.add("show");
+      entry.target.classList.add('show');
     } else {
-      entry.target.classList.remove("show");
+      entry.target.classList.remove('show');
     }
   });
 });
 
-const hiddenElements = document.querySelectorAll(".hiddenleft");
+const hiddenElements = document.querySelectorAll('.hiddenleft');
 hiddenElements.forEach((el) => observer.observe(el));
 
-const hiddenElementscenter = document.querySelectorAll(".hiddencenter");
+const hiddenElementscenter = document.querySelectorAll('.hiddencenter');
 hiddenElementscenter.forEach((el) => observer.observe(el));
+
+function pindahWisata() {
+  window.location.href = '/Wisata/index.html';
+}
+function pindahKuliner() {
+  window.location.href = '/Makanan/makanan.html';
+}
